24b5c8e06c4c4c1be9e2302949cdc61c82c044ed
Behzad <behzad.haghgoo@gmail.com>
2021-07-30 14:30:20 -0700

    WIP

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ def less_frequent_job():
     print("I'm working... occassionally...")
 
 
-def less_frequent_job():
-    print("I'm working... occassionally...")
-
-
 def main():
     schedule.every(1).seconds.do(job)
     schedule.every(5).seconds.do(less_frequent_job)
