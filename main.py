0efd76ca393dd98d575e51406f54e05518850dc5
Behzad <behzad.haghgoo@gmail.com>
2021-07-30 14:41:19 -0700

    WIP

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ def experimental_function(experiment):
     return experiment
 
 
-def less_frequent_job():
-    print("I'm working... occassionally...")
-
-
 def main():
     schedule.every(1).seconds.do(job)
     schedule.every(5).seconds.do(less_frequent_job)
