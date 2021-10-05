306c042f524c8010383186adf51df2235639a3ea
Behzad <behzad.haghgoo@gmail.com>
2021-10-05 15:37:32 -0700

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import schedule
 import time
 
+
+def experimental_function(experiment):
+    print("experiment: {experiment}")
+    return experiment
+
+
+def less_frequent_job():
+    print("I'm working... occassionally...")
+
+
+
 def main():
     schedule.every(1).seconds.do(job)
     schedule.every(5).seconds.do(less_frequent_job)
