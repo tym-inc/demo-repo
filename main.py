6a6c70713d321108473713fd4839caf1f7d4eef6
Seah Ying Hang <seahyinghang8@gmail.com>
2022-04-12 19:04:55 -0700

    python test code

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-import schedule
-import time
 
-
-
-def test():
-    print('hi')
-
-
-
-def less_frequent_job():
-    print("I'm working... occassionally...")
+import numpy as np
 
 def main():
-    schedule.every(1).seconds.do(job)
-    schedule.every(5).seconds.do(less_frequent_job)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
+    print('hello world')
+    print(np.zeros((2, 3)))
 
 if __name__ == "__main__":
     main()
---
+++ utils/helper.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+    chk_map = {}
+    for index, val in enumerate(nums):
+        compl = target - val
+        if compl in chk_map:
+            indices = [chk_map[compl], index]
+            return [indices]
+        else:
+            chk_map[val] = index
+    return False
+
+print(twoSum([12, 53, 64, 67, 2, 1, 3, 4, 6], 68))
